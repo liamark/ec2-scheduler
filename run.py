59229a7111837687a03f244871f8a7e29f44e4b2
Ben Fairless <ben.fairless@landregistry.gov.uk>
2016-09-07 13:08:20 +0100

    Added Slack support

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from ec2 import EC2Instance
 import boto3
+import os
 
 ################################################################################
 ############################ LOGGING CONFIGURATION #############################
@@ -14,6 +15,84 @@ logger = logging.getLogger('EC2 Manager')
 logging.getLogger("botocore").setLevel(logging.ERROR)
 logging.getLogger("boto3").setLevel(logging.ERROR)
 
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+################################################################################
+############################## SLACK INTEGRATION ###############################
+################################################################################
+
+import requests
+import json
+slack_token=os.getenv('SLACK_TOKEN')
+def slackpost(message):
+    url = 'https://hooks.slack.com/services/' + slack_token
+    headers = {
+        'user-agent': 'ec2-scheduler',
+        'content-type': 'application/json'
+    }
+    payload = {
+        'username': 'Schedulebot',
+        'icon_emoji': ':robot_face:',
+        'message': 'Changes have been made to the AWS infrastructure.',
+        'mrkdwn': True,
+        'attachments': [
+            {
+                'pretext': 'Changes have been made to the AWS infrastructure.',
+                'color': '#78D0DE',
+                'text': message,
+                'mrkdwn_in': [
+                    'text',
+                    'pretext'
+                ]
+            }
+        ]
+    }
+    slack = requests.post(url, data=json.dumps(payload), headers=headers)
+    if slack.status_code == 200:
+        logger.info('Submitted results via Slack.')
+    else:
+        logger.error('Failed to send message to Slack!')
+    return
+
+################################################################################
+############################## CHANGES REPORTING ###############################
+################################################################################
+
+# In order to track changes made we'll populate a dictionary of changes
+changes = {
+    'started': [],
+    'stopped': [],
+    'terminated': []
+}
+
+def report():
+    """ Generates simple report of changes made during run """
+    response = ''
+    if changes['started']:
+        response += '*The following instances were started:*\n'
+        results = []
+        for instance in changes['started']:
+            text = '%s *(%s)*' % (instance.properties['Name'], instance.id)
+            results.append(text)
+        response += '\n'.join(results) + '\n'
+    if changes['stopped']:
+        response += '*The following instances were stopped:*\n'
+        results = []
+        for instance in changes['stopped']:
+            text = '%s *(%s)*' % (instance.properties['Name'], instance.id)
+            results.append(text)
+        response += '\n'.join(results) + '\n'
+    if changes['terminated']:
+        response += '*The following instances were terminated:*\n'
+        results = []
+        for instance in changes['terminated']:
+            text = '%s *(%s)*' % (instance.properties['Name'], instance.id)
+            results.append(text)
+        response += '\n'.join(results) + '\n'
+    if len(response) == 0:
+        response = False
+    return response
 
 ################################################################################
 ################################## FUNCTIONS ###################################
@@ -24,9 +103,11 @@ def checkSchedule(EC2Instance):
     if EC2Instance.managed:
         if EC2Instance.state['Name'] == 'stopped' and EC2Instance.scheduled:
             EC2Instance.start()
+            changes['started'].append(EC2Instance)
             logger.info('%s - Instance started - %s' % (EC2Instance.id, EC2Instance.properties['Name']))
         elif EC2Instance.state['Name'] == 'running' and not EC2Instance.scheduled:
             EC2Instance.stop()
+            changes['stopped'].append(EC2Instance)
             logger.info('%s - Instance stopped - %s' % (EC2Instance.id, EC2Instance.properties['Name']))
     return None
 
@@ -47,6 +128,7 @@ def checkExpires(EC2Instance):
         EC2Instance.properties['Tag'] = 'Value'
         logger.warn('%s - Instance expired - %s' % (EC2Instance.id, EC2Instance.properties['Name']))
         # EC2Instance.terminate()
+        changes['terminated'].append(EC2Instance)
         logger.warn('%s - Instance terminated - %s' % (EC2Instance.id, EC2Instance.properties['Name']))
     return None
 
@@ -68,3 +150,6 @@ if __name__ == '__main__':
         checkCreator(i)
         checkSchedule(i)
     logger.info('Scheduler completed.')
+    result = report()
+    if result and slack_token:
+        slackpost(result)
